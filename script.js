0f5b5e00d1349304939b9c5607c1ef45da4e2eed
GautamPB <pbg2402@gmail.com>
2021-01-15 22:56:54 +0530

    Fixed a bug which appears after winning

--- script.js
+++ script.js
@@ -17,24 +17,36 @@ const playAgain = () => {
     finalScore = ''
     document.getElementById('ans').innerHTML = '?'
     document.body.style.backgroundColor = '#404040'
+    document.getElementById('check').disabled = false
 }
 
 const checkNumber = () => {
-    const guessedNumber = document.querySelector('.input').value
-    if (guessedNumber == numberToGuess) {
+    const guessedNumber = Number(document.querySelector('.input').value)
+
+    if (!guessedNumber) {
+        document.getElementById('message').innerHTML = '🚫 Not a number!'
+        score -= 1
+        finalScore += '💯 Score: ' + score
+        document.getElementById('score').innerHTML = finalScore
+        finalScore = ''
+        console.log(guessedNumber)
+    }
+
+    if (guessedNumber === numberToGuess) {
         document.getElementById('message').innerHTML = '🎉That is Correct!'
         highscore += '🥇 Highscore: ' + score
         document.getElementById('highscore').innerHTML = highscore
         highscore = ''
         document.getElementById('ans').innerHTML = guessedNumber
         document.body.style.backgroundColor = '#17E217'
+        document.getElementById('check').disabled = true
     } else if (guessedNumber > numberToGuess) {
         document.getElementById('message').innerHTML = 'Too High!'
         score -= 1
         finalScore += '💯 Score: ' + score
         document.getElementById('score').innerHTML = finalScore
         finalScore = ''
-    } else {
+    } else if (guessedNumber < numberToGuess) {
         document.getElementById('message').innerHTML = 'Too Low!'
         score -= 1
         finalScore += '💯 Score: ' + score
