39342c967c01ea48d8364a25085a876552f76934
GautamPB <pbg2402@gmail.com>
2021-01-15 16:33:19 +0530

    Finished the game

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
 //Guess my number!
-'use strict';
+'use strict'
+
+let finalScore = ''
+let score = 20
+
+let highscore = ''
+
+let numberToGuess = Math.trunc(Math.random() * 20) + 1
+
+const playAgain = () => {
+    numberToGuess = Math.trunc(Math.random() * 20) + 1
+    console.log(numberToGuess)
+    score = 20
+    finalScore += '💯 Score: ' + score
+    document.getElementById('score').innerHTML = finalScore
+    finalScore = ''
+    document.getElementById('ans').innerHTML = '?'
+    document.body.style.backgroundColor = '#404040'
+}
+
+const checkNumber = () => {
+    const guessedNumber = document.querySelector('.input').value
+    if (guessedNumber == numberToGuess) {
+        document.getElementById('message').innerHTML = '🎉That is Correct!'
+        highscore += '🥇 Highscore: ' + score
+        document.getElementById('highscore').innerHTML = highscore
+        highscore = ''
+        document.getElementById('ans').innerHTML = guessedNumber
+        document.body.style.backgroundColor = '#17E217'
+    } else if (guessedNumber > numberToGuess) {
+        document.getElementById('message').innerHTML = 'Too High!'
+        score -= 1
+        finalScore += '💯 Score: ' + score
+        document.getElementById('score').innerHTML = finalScore
+        finalScore = ''
+    } else {
+        document.getElementById('message').innerHTML = 'Too Low!'
+        score -= 1
+        finalScore += '💯 Score: ' + score
+        document.getElementById('score').innerHTML = finalScore
+        finalScore = ''
+    }
+}
